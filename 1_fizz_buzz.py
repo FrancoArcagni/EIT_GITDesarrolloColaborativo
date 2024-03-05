11728112ca4c91f07740cc85a274d1caee654e39
Franco <francoarcagni@hotmail.com>
2024-03-05 11:12:38 -0300

    Nueva Prueba

---
+++ 1_fizz_buzz.py
@@ -0,0 +1,23 @@
+#1
+"""
+EL FAMOSO "FIZZ BUZZ"
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+ """
+ 
+numeros = range (1,101)
+for cada_numero in numeros:
+    if cada_numero %3 == 0 and cada_numero %5 ==0:
+        print ("fizzbuzz")
+    elif cada_numero %5 ==0:
+        print ("buzz")
+    elif cada_numero %3 == 0:
+        print ("fizz")
+    else:
+        print (cada_numero)
